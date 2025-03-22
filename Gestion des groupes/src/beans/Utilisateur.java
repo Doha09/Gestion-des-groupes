bc88d499f249f7b6f56604a06bbf7c1209cfa2e5
Doha09 <ouahmanedoha03@gmail.com>
2025-03-22 15:26:53 +0000

    Add files via upload

---
+++ Gestion des groupes/src/beans/Utilisateur.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author ouahm
+ */
+public class Utilisateur {
+
+    private int id;
+    private String login;
+    private String password;
+    private String role; // Utilisation d'un String au lieu d'une Enum
+
+    public Utilisateur(int id, String login, String password, String role) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Utilisateur(String login, String password, String role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+}
+
