0bef71e12c512c55ff9fd1c8b8523fbb9d6a3e6c
Doha09 <ouahmanedoha03@gmail.com>
2025-03-22 01:03:54 +0000

    Add files via upload

---
+++ Gestion des groupes/src/beans/Grpage.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author ouahm
+ */
+public class Grpage {
+
+    private int id;
+    private String nomGroupe;
+    private String niveau;
+    private String filiere;
+    private int effectif;
+
+    public Grpage(int id, String nomGroupe, String niveau, String filiere, int effectif) {
+        this.id = id;
+        this.nomGroupe = nomGroupe;
+        this.niveau = niveau;
+        this.filiere = filiere;
+        this.effectif = effectif;
+    }
+
+    public Grpage(String nomGroupe, String niveau, String filiere, int effectif) {
+        this.nomGroupe = nomGroupe;
+        this.niveau = niveau;
+        this.filiere = filiere;
+        this.effectif = effectif;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNomGroupe() {
+        return nomGroupe;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public String getFiliere() {
+        return filiere;
+    }
+
+    public int getEffectif() {
+        return effectif;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNomGroupe(String nomGroupe) {
+        this.nomGroupe = nomGroupe;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+
+    public void setFiliere(String filiere) {
+        this.filiere = filiere;
+    }
+
+    public void setEffectif(int effectif) {
+        this.effectif = effectif;
+    }
+
+    @Override
+    public String toString() {
+        return  nomGroupe ;
+    }
+
+    
+}
